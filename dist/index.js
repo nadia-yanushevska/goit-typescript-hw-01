215799767f2d438080f5738e69f079e5c92f17d5
Nadia <nadiazlagoda@gmail.com>
2024-04-23 18:10:14 +0300

    concatenation, index

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
+import { concatenation } from './concatenation';
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+if (input && button) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello!');
+    });
+}
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { concatenation } from './concatenation';
+
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
+
+if (input && button) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello!');
+    });
+}
